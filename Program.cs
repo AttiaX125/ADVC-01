0411ca521ddc821a236f372289637d93c00deaba
AttiaX125 <AttiaX125@gmail.com>
2026-09-13 09:21:09 +0300

    Question01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
+        #region Q1 - Generic Class
+        /*
+        public class Box<T>
+        {
+            private T _value;
+
+            public void Set(T value) => _value = value;
+            public T Get() => _value;
+        }
+        */
+        #endregion
     }
 }
