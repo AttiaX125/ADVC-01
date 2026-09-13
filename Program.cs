40ef10b26cc8c272f2a166e7b2db10b0f82ccc56
AttiaX125 <AttiaX125@gmail.com>
2026-09-13 09:28:50 +0300

    Q14

--- Program.cs
+++ Program.cs
@@ -145,5 +145,24 @@
             return default(T);
         }
         #endregion
+        #region Q14 - SafeList<T>
+        public class SafeList<T>
+        {
+            private List<T> items = new List<T>();
+
+            public void Add(T item)
+            {
+                items.Add(item);
+            }
+
+            public T Get(int index)
+            {
+                if (index < 0 || index >= items.Count)
+                    return default(T);
+
+                return items[index];
+            }
+        }
+        #endregion
     }
 }
